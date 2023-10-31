9cbc4461046a74ae6e0a2931d87de00fc1546fb5
AC <obiakachukwu@gmail.com>
2023-10-30 22:55:51 -0400

    e14 working changes

--- py/e14.py
+++ py/e14.py
@@ -2,35 +2,39 @@
 # Project Euler #14 - Longest Collatz sequence (phi function)
 # Solved July 22, 2018
 
-def phi_function(n):
+def phiFunction(number):
 	""" generates a Collatz sequence given a starting number"""
-	result = [n]
-	while n > 1:
-		if n % 2 == 0:
-			n = n / 2
+	result = [number]
+	while number > 1:
+		if number % 2 == 0:
+			number = number // 2
 		else:
-			n = 3 * n + 1
-		result.append(n)
-
+			number = 3 * number + 1
+		result.append(number)
 	return result
-	# return len(result)
-# test
-# print(phi_function(16))
+# print(phiFunction(16))
 
-def longest_chain_under(m):
+# just focusing on length using recursion
+def lenPhiSet(number):
+    """returns length of a Collatz sequence starting at number"""
+    if number == 1:
+        return number
+    elif number % 2 == 0:
+        return 1 + lenPhiSet(number // 2)
+    else:
+        return 1 + lenPhiSet(3*number + 1)
+print(lenPhiSet(16))
+
+def longestChainUnder(number):
 	""" find the longest collatz sequence under a given number limit """
-
 	maxLength = 0
-
-	for i in range(m/2 + 1, m, 2): # assuming number would be odd
-		length = len(phi_function(i))
+	for i in range(number//2 + 1, number, 2): # assuming number would be odd
+		length = len(phiFunction(i))
 		if length > maxLength:
 			maxLength = length
 			result = i
-
 	return str(result) + ": " + str(maxLength) + " members"
-
-print(longest_chain_under(1000000)) # 837799: 525 members; finished in 11.6 s
+print(longestChainUnder(1000000)) # 837799: 525 members; finished in 11.6 s
 
 
 """ with several useful optimizations, we can shorten calculation time """
--- py/e9.py
+++ py/e9.py
@@ -1,13 +1,62 @@
 # Akachukwu Obi, 2018
 # Project Euler #9
 
-def pythagoreanTripleSum(upTo):
-	for a in range(3, (upTo - 3) / 3):
-		for b in range(a + 1, (upTo - a) / 2):
-			c = upTo - b - a
-			if c * c == (a * a) + (b * b):
-				# return a * b * c
-				return str(a) + ' * ' + str(b) + ' * ' + str(c) + ' = ' + str(a * b * c)
-	return "I tried ..." # if nothing is found
-# test
-print(pythagoreanTripleSum(1000)) # 31875000
+# Solution 1 based on definition of pythagorean triplets
+def pythagoreanTripleSum(upTo=1000):
+    """find pythagorean triplet a+b+c = upTo, return a*b*c if found"""
+    for a in range(3, (upTo - 3) // 3):
+        for b in range(a + 1, (upTo - a) // 2):
+            c = upTo - b - a
+            if c * c == (a * a) + (b * b):
+                # return a * b * c
+                return str(a) + ' * ' + str(b) + ' * ' + str(c) + ' = ' + str(a * b * c)
+    return "I toiled all night and found nothing"
+print(pythagoreanTripleSum()) # 200 * 375 * 425 = 31875000
+print(pythagoreanTripleSum(237)) # I toiled all night and found nothing
+
+#solution 1b modifed for multiples 
+from math import sqrt
+def pythagoreanTripleSum(upTo=1000):
+    """find pythagorean triplet a+b+c = upTo, return a*b*c if found"""
+    for a in range(3, (upTo - 3) // 3):
+        for b in range(a + 1, (upTo - a) // 2):
+            c = sqrt((a * a) + (b * b))
+            if c == int(c): # if c is a rational number
+                c = int(c)
+                S = a + b + c
+                if S == upTo:
+                    return f"{a} * {b} * {c} = {a * b * c}"
+                elif upTo % S == 0:
+                    k = upTo // S
+                    return f"{k*a} * {k*b} * {k*c} = {k**3 * a * b * c}"
+    return "I toiled all night and found nothing"
+print(pythagoreanTripleSum()) # 200 * 375 * 425 = 31875000
+
+
+# Solution 2 using parameterization
+import math
+
+def pythagoreanTripletSumTo(total=1000):
+    """find pythagorean triplet a+b+c = total, return a*b*c if found
+        script based on the parametrization of pythagorean triplets"""
+    mlimit = int(math.sqrt(total/2)) + 1
+    for m in range(2, mlimit): # starts at 2 to ensure nrange is a range
+        if m % 2 == 0: # if even
+            nrange = range(1, m, 2) # make n odd
+        else:
+            nrange = range(2, m, 2) # else make n even
+        for n in nrange:
+            # formula below always gives a,b,c as pythagorean triplets
+            a = m**2 - n**2
+            b = 2 * m * n
+            c = m**2 + n**2 
+            if a + b + c == total:
+                return f"{a} * {b} * {c} = {a * b * c}"
+            elif total % (a + b + c) == 0: # checks for non primitive sets
+                k = total // (a + b + c)
+                return f"{k*a} * {k*b} * {k*c} = {k**3 * a * b * c}"
+    return "I toiled all night and found nothing"
+
+print(pythagoreanTripletSumTo()) # 375 * 200 * 425 = 31875000
+print(pythagoreanTripletSumTo(200)) # 75 * 40 * 85 = 255000
+# total = 200 returns "I toiled..." without the elif script
